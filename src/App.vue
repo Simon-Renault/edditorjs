52453dba9c8466c3a5fed90a773364b8d3046da5
simon <simon.renault.pro@gmail.com>
2020-06-14 11:55:45 +0100

    test

--- src/App.vue
+++ src/App.vue
@@ -5,5 +5,43 @@
 </template>
 
 <style lang="scss">
-
+:root {
+    --geist-foreground: #000;
+    --geist-background: #fff;
+    --geist-selection: var(--geist-cyan);
+    --accents-1: #fafafa;
+    --accents-2: #eaeaea;
+    --accents-3: #999;
+    --accents-4: #888;
+    --accents-5: #666;
+    --accents-6: #444;
+    --accents-7: #333;
+    --accents-8: #111;
+    --geist-link-color: var(--geist-success);
+    --geist-marketing-gray: #fafbfc;
+    --geist-code: var(--geist-purple);
+    --geist-success-light: #3291ff;
+    --geist-success: #0070f3;
+    --geist-success-dark: #0366d6;
+    --geist-error-light: #ff1a1a;
+    --geist-error: #e00;
+    --geist-error-dark: #c00;
+    --geist-warning-light: #f7b955;
+    --geist-warning: #f5a623;
+    --geist-warning-dark: #f49b0b;
+    --geist-secondary-light: var(--accents-3);
+    --geist-secondary: var(--accents-5);
+    --geist-secondary-dark: var(--accents-7);
+    --dropdown-box-shadow: 0 4px 4px 0 rgba(0,0,0,0.02);
+    --dropdown-triangle-stroke: #fff;
+    --scroller-start: #fff;
+    --scroller-end: hsla(0,0%,100%,0);
+    --shadow-smallest: 0px 4px 8px rgba(0,0,0,0.12);
+    --shadow-small: 0 5px 10px rgba(0,0,0,0.12);
+    --shadow-medium: 0 8px 30px rgba(0,0,0,0.12);
+    --shadow-large: 0 30px 60px rgba(0,0,0,0.12);
+    --shadow-hover: 0 30px 60px rgba(0,0,0,0.12);
+    --shadow-sticky: 0 12px 10px -10px rgba(0,0,0,0.12);
+    --portal-opacity: 0.25;
+}
 </style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,36 +1,194 @@
 <template>
-  <a href="https://github.com/login/oauth/authorize?scope=repo&client_id=bf5071b7cb30d7e2ad66">login with github</a>
+<div class="app">
+  <div class="repos">
+    <a href="https://github.com/login/oauth/authorize?scope=repo&client_id=bf5071b7cb30d7e2ad66">login with github</a>  
+    <ul>
+      <li v-for="(repo,i) in repos" :key="'repo'+ i" @click="selectRepo(repo)">
+        <div>{{repo.name}}</div>
+        <div>{{repo.size}}</div>
+      </li>
+    </ul>
+  </div>
+
+  <div class="files">
+    <div>
+       Selected repo : {{selectedRepo}}
+    </div>
+    <ul>
+      <li v-for="(file,i) in selectedRepoContent" :key="'repo'+ i" @click="selectFile(file)">{{file.name}}</li>
+    </ul>
+  </div>
+
+  <div class="editor">
+    <div>
+
+    </div>
+    Selected file : {{selectedFile}}
+    <input type="text" name="" id="" v-model="selectedFile">
+    <input type="text" name="" id="" v-model="fileSha">
+    <textarea name="" id="" cols="30" rows="40" v-model="fileContent"></textarea>
+    <button @click="createFile">new file</button>
+  </div>
+  
+
+ 
+ 
+
+  
+</div>
+
 </template>
 
 <script>
 import GitHub from 'github-api';
 import Axios from 'axios'
+const { Octokit } = require("@octokit/rest");
+
 
 export default {
   data(){
     return{
-      token : null
+      token : null,
+      user : null,
+      repos : [],
+      selectedRepo : null,
+      selectedRepoContent : null,
+      selectedFile:null,
+      fileContent : null,
+      fileSha : null,
+      fileTitle : null
+    }
+  },
+  methods : {
+    async selectRepo(repo){
+      console.log(repo)
+      this.selectedRepo = repo.name
+      const content = await this.octokit.repos.getContent({
+        owner: this.user.login,
+        repo: repo.name,
+        path: ''
+      })
+      this.selectedRepoContent = content.data.filter(c => {
+        var re = /(?:\.([^.]+))?$/;
+        
+        const isFile = c.type === 'file' 
+        if(!isFile) return false
+        const ext = re.exec(c.path)[1];  
+        console.log(ext)
+        const whitelist = ['md','html']
+        const hasRightExtension = whitelist.includes(ext)
+        if(hasRightExtension  ){
+          return true
+        }
+        return false
+      })
+      console.log(content)
+    },
+    async selectFile(file){
+      this.selectedFile = file.path
+      const content = await this.octokit.request(`GET /repos/${this.user.login}/${this.selectedRepo}/contents/${this.selectedFile}`,{
+        ref:'master'
+      })
+      this.fileSha = file.sha
+      this.fileContent = atob(content.data.content)
+    },
+    async createFile(){
+        const repos  = await this.octokit.request(`PUT /repos/${this.user.login}/${this.selectedRepo}/contents/${this.selectedFile}`,{
+          "message": "my commit message",
+          "committer": {
+            "name": this.user.name,
+            "email": this.user.email
+          },
+          "content": btoa(this.fileContent),
+          "sha": this.fileSha
+        })
     }
   },
   mounted(){
     this.$nextTick( async () => {
       let code =  this.$route.query.code
       if(!code) return
-      console.log(code)
-      const token = await Axios.get('https://edditorjs.vercel.app/api/login?code=' + code)
-      this.token = token.data
-      console.log(token)
 
-      const gh = new GitHub({token});
+      if(!localStorage.getItem('token')){
+        const token = await Axios.get('https://edditorjs.vercel.app/api/login?code=' + code)
+        this.token = token.data
+        localStorage.setItem('token',this.token)
+      }else{
+        this.token = localStorage.getItem('token')
+      }
 
-      console.log(gh)
-      
+    
+      this.octokit = new Octokit({
+        auth: this.token
+      });
+
+      const rate = await this.octokit.rateLimit.get();
+      console.log(rate)
+      const { data } = await this.octokit.request("/user");
+      this.user = data
+      const repos  = await this.octokit.request('GET /user/repos')
+      this.repos =  repos.data.filter(repo => repo.size > 0)
+     
     })
    
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+*,*::before,*::after{
+  padding:0;
+  margin:0;
+  box-sizing: border-box;
+  font-family: sans-serif;
+}
+li{
+  list-style: none;
+}
+ul{
 
+}
+.app{
+  display:grid;
+  grid-template-columns: 300px 300px 1fr;
+  height:100vh;
+}
+.repos{
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  background : var(--accents-1);
+  border-right :  1px solid var(--accents-2);
+  max-height: 100vh;
+  overflow: auto;
+  a{
+    background : var(--accents-8);
+    width:100%;
+    height: 50px;
+    color: var(--accents-1);
+    text-decoration: none;
+    flex-shrink: 0;
+  }
+  li{
+    padding: 10px;
+    cursor: pointer;
+     border-bottom :  1px solid var(--accents-2);
+  }
+}
+.files{
+     background : var(--accents-1);
+  border-right :  1px solid var(--accents-2);
+   display:flex;
+  flex-direction: column;
+   li{
+    padding: 10px 10px;
+    cursor: pointer;
+     border-bottom :  1px solid var(--accents-2);
+  }
+}
+.editor{
+  padding: 10%;
+  display:flex;
+  flex-direction: column;
+}
 </style>
